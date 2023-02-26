5ac415c21b1a4deb4a56588f151bde230ef74338
Mateo Ortiz <mateo972000@gmail.com>
2023-02-26 10:43:46 -0500

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Mateo Ortiz on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
